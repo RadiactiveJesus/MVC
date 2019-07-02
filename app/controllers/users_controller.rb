ef0ebb1ffa99c10e6eb44a6c1a02d7d671bc9b6b
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-07-02 09:09:25 -0500

    GET Requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,15 @@ class UsersController < ApplicationController
     end
 
     def show
+        @name = "I am the Show action!"
     end
 
     def new
+        @name = "I am the New action!"
     end
 
     def edit
+        @name = "I am the Edit action!"
     end
       
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,4 +2,6 @@ require 'rest-client'
   url = "http://localhost:3000/users"
 
   puts RestClient.get(url)
-  puts RestClient.get('http://localhost:3000/users', {params: {id: 1}})
+  puts RestClient.get("http://localhost:3000/users/:id", {params: {id: 1}})
+  puts RestClient.get("http://localhost:3000/users/new")
+  puts RestClient.get("http://localhost:3000/users/:id/edit", {params: {id: 1}})
